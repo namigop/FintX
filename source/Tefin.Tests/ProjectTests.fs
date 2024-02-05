f23a6ea8253681d818c7fd61c91e2ee6e96ea208
namigop <erik.araojo@wcfstorm.com>
2024-02-05 17:46:57 +0800

    unit test in progress

--- source/Tefin.Tests/ProjectTests.fs
+++ source/Tefin.Tests/ProjectTests.fs
@@ -1,10 +1,12 @@
 ﻿module ProjectTests
 
 open System
+open System.Text.RegularExpressions
 open Microsoft.VisualBasic.FileIO
 open Tefin.Core
 open Tefin.Core.Interop
 open Xunit
+open System.IO;
 
 
 let testProjectName = "myProject"
@@ -13,6 +15,9 @@ let testMethodName1 = "myMethod1"
 let testMethodName2 = "myMethod1"
 let testFile1Request = "file1.fxrq"
 let testFile1Content = "{}"
+
+let testProjSaveStateCotent = "{}"
+let testClientConfigContent = "{}"
 
 let testFile2Request = "file2.fxrq"
 let testFile2Content = "{}"
@@ -26,7 +31,7 @@ type File =
 type Folder =
   {
     Name : string
-    Files : string array
+    Files : File array
     Folders : Folder array
   }
 let projectFolder =
@@ -50,15 +55,21 @@ let projectFolder =
     Folders = [|
       {
         Name = testProjectName
-        Files = [| ProjectSaveState.FileName |]
+        Files = [| 
+                    { Name = ProjectSaveState.FileName; Content =  testProjSaveStateCotent} |]
         Folders = [|
           {
             Name = testClientName
-            Files = [| ClientGroup.ConfigFilename |]
+            Files = [|
+              { Name =ClientGroup.ConfigFilename; Content = testClientConfigContent }
+            |]
             Folders = [|
               {
                 Name = "code"
-                Files = [| "Client.cs"; "Client.g.cs" |]
+                Files = [|
+                  { Name = "Client.cs"; Content = "not used" }
+                  { Name = "Client.g.cs"; Content = "not used" }
+                |]
                 Folders = Array.empty
               }
               {
@@ -67,7 +78,9 @@ let projectFolder =
                 Folders = [|
                   {
                     Name = testMethodName1
-                    Files = [|testFile1Request|]
+                    Files = [|
+                      { Name = testFile1Request; Content =  testFile1Content}
+                    |]
                     Folders = [|
                       {
                         Name= Project.AutoSaveFolderName
@@ -78,7 +91,9 @@ let projectFolder =
                   }
                   {
                     Name = testMethodName2
-                    Files = [|testFile2Request|]
+                    Files = [|
+                       { Name = testFile2Request; Content =  testFile2Content}
+                    |]
                     Folders = [|
                       {
                         Name= Project.AutoSaveFolderName
@@ -99,34 +114,85 @@ let projectFolder =
   
  
 let mock =
-  let rec getFilesRec (folder:Folder) (option:SearchOption) (pathParts:string array) (p:string)  =
-    if option = SearchOption.SearchTopLevelOnly then
-      if (pathParts[pathParts.Length-1] = folder.Name) then
-        folder.Files
+  let matchFilePattern (file: File) (pattern:string) =
+      if (pattern = "*.*") then
+        true
+      else
+        let ext = Path.GetExtension file.Name
+        file.Name.EndsWith(ext)
+      
+  let rec getDirRec (folder:Folder) (targetPath:string) (currentPath:string)  =
+      if currentPath = targetPath then
+        folder.Folders      
       else        
           folder.Folders
-          |> Array.collect (fun f -> getFilesRec f option pathParts "")
+          |> Array.collect (fun f -> getDirRec f targetPath $"{currentPath}/{f.Name}")
+       
+  let rec getFilesRec (folder:Folder) pattern (option:SearchOption) (pathParts:string array) (p:string)  =
+    if option = SearchOption.TopDirectoryOnly then
+      if (pathParts[pathParts.Length-1] = folder.Name) then
+        folder.Files
+        |> Array.filter (fun f -> matchFilePattern f pattern)
+        |> Array.map(fun f ->
+             let p = String.Join("/", pathParts)
+             let fullPath = $"{p}/{f.Name}"
+             {f with Name = fullPath }
+             )
+      else        
+          folder.Folders
+          |> Array.collect (fun f -> getFilesRec f pattern option pathParts "")
+          |> Array.filter (fun f -> matchFilePattern f pattern)
     else       
         folder.Files
-        |> Array.map(fun f -> $"{p}/{f}")
+        |> Array.filter (fun f -> matchFilePattern f pattern)
+        |> Array.map(fun f ->
+             let fullPath = $"{p}/{f.Name}"
+             {f with Name = fullPath }
+             )
         |> Array.append (
           folder.Folders
-          |> Array.collect (fun f -> getFilesRec f option pathParts ($"{p}/{f.Name}") ))         
+          |> Array.collect (fun f -> getFilesRec f pattern option pathParts ($"{p}/{f.Name}") ))         
            
   let getFiles (path:string, pattern:string, options:SearchOption) =
     let pathParts = path.Split("/")
-    getFilesRec projectFolder options pathParts projectFolder.Name
+    getFilesRec projectFolder pattern options pathParts projectFolder.Name
     
   let readAllText (file:string) =
+    let dir = Path.GetDirectoryName file
+    getFiles (dir, "*.*", SearchOption.AllDirectories)
+    |> Array.tryFind(fun f -> f.Name = file)
+    |> fun m ->
+      match m with
+      | Some(f) -> f.Content
+      | None -> raise (FileNotFoundException("file not found", file))
+      
+  let createDirectory name =
+    ()
+  
+  let getDirectories (targetPath:string) =
+    getDirRec projectFolder targetPath ""
+  
+  getDirectories
+                                                                                 
+                                                                                    
+      
+      
+    
     
 
   
-
+(* (createDirectory: string -> unit)
+    (getDirectories: string -> string array)*)
 [<Fact>]
 let ``Can build class instance`` () =
-  let duh =
-    mock($"projects/{testProjectName}/{testClientName}", "*.*", SearchOption.SearchAllSubDirectories)
-  Assert.True(duh.Length > 1)
+  // let file = $"projects/{testProjectName}/{testClientName}/methods/{testMethodName1}/{testFile1Request}"
+  // let content = mock file
+  //
+   let dirs = mock ($"projects/{testProjectName}/{testClientName}/methods")
+   ()
+  // let duh =
+  //   mock($"projects/{testProjectName}/{testClientName}", "*.*", SearchOption.SearchAllSubDirectories)
+  // Assert.True(duh.Length > 1)
 
 
 
