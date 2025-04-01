f237c253672b82a23fed759a52e577aaea824abe
erik.araojo <erik.araojo@cartrack.com>
2025-04-02 01:01:35 +0800

    new env var screen

---
+++ source/Tefin/Views/ProjectEnv/NewEnvVariableContext.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Tefin.Views.ProjectEnv;
+
+public partial class NewEnvVariableContext : UserControl {
+    public NewEnvVariableContext() {
+        InitializeComponent();
+    }
+}
